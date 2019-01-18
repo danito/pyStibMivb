34a054fc985d1bfb2edc95416754130d76c2e246
Daniel nix <daniel.nix@gmail.com>
2019-01-19 00:57:52 +0100

    Update setup.py
    correct classifier

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ),
 )
