1be0f97a4abdc36360163aff389e249dbb91f6d1
Daniel Nix <daniel.nix@gmail.com>
2019-01-19 23:18:08 +0100

    improving test file

--- tests/getstibdata.py
+++ tests/getstibdata.py
@@ -1,14 +1,39 @@
-from stibmivb import iStibmivb
+from pystibmivb import iStibmivb
 import xmltodict
-import pprint
-stib = iStibmivb()
+from collections import OrderedDict 
 
+latitude = '50.809655'
+longitude = '4.320166'
+lang = 'fr'
 
-mystops = stib.get_lines_new()
-dd = xmltodict.parse(mystops)
-print (dd['lines']['line'][0])
+stib = iStibmivb(lang)
+stibdata = {}
 
-mystops = stib.get_close_stops('50.807736','4.313616')
-print (mystops.decode())
-co = xmltodict.parse(mystops)
-pprint.pprint(co)
+mystops = stib.get_close_stops(latitude,longitude)
+stops = xmltodict.parse(mystops)
+
+for halt in stops['halts']['halt']:
+    print(halt['name'], halt['id'])
+    lines = halt['destinations']['destination']
+    stop_id = halt['id']
+    stop_name = halt['name']
+    if type(lines) is OrderedDict:
+        print(lines['mode'],lines['line'],lines['name'],lines['destcode'])
+        waiting_times = stib.get_waiting_times(lines['line'],lines['destcode'],stop_id)
+        waiting_times = xmltodict.parse(waiting_times)
+        wt =(waiting_times['waitingtimes']['waitingtime'])
+        for w in wt:
+            if w['line'] == lines['line']:
+                print("    ",w['minutes'], w['destination'])
+    else:
+       for l in lines:
+          print (l['mode'],l['line'],l['name'],l['destcode'])
+          waiting_times = stib.get_waiting_times(l['line'],l['destcode'],stop_id)
+          waiting_times = xmltodict.parse(waiting_times)
+          wt =(waiting_times['waitingtimes']['waitingtime'])
+          for w in wt:
+            if w['line'] == l['line']:
+                print("    ",w['minutes'], w['destination'])
+          
+    print ("================================")
+    
