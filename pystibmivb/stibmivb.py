245028fd82252406039306db7b88f3f22b94612a
danito <daniel.nix@gmail.com>
2019-01-31 11:15:29 +0100

    handle incomplete xml response

--- pystibmivb/stibmivb.py
+++ pystibmivb/stibmivb.py
@@ -1,5 +1,6 @@
 import requests
 import xmltodict
+import json
 
 
 session = requests.Session()
@@ -63,8 +64,11 @@ class Stibmivb:
               try:
                   response = session.get(url, params = params, headers = headers)
                   try:
-                      xml_data = response.content
-                      return xml_data
+                      response_data = response.content
+                      if self.format is 'json':
+                          print('format : json')
+                          response_data = self.get_json(response_data)
+                      return response_data
                   except ValueError:
                       return -1
               except requests.exceptions.RequestException as e:
@@ -82,24 +86,36 @@ class Stibmivb:
           """Retrieve a list of stops near a waypoint"""
           if latitude is not None and longitude is not None:
               extra_params = {'latitude': latitude, 'longitude':longitude}
-              xml_data = self.do_request('getclosestops', extra_params)
-              return xml_data
+              response_data = self.do_request('getclosestops', extra_params)
+              return response_data
 
       def get_lines_new(self):
           """Retrieve all lines."""
-          xml_data = self.do_request('getlinesnew')
-          return xml_data
+          response_data = self.do_request('getlinesnew')
+          return response_data
 
       def get_waiting_times(self, line=None, iti=1, halt=None):
           '''Retrieve waiting times for a line at a stop id'''
           if None not in (line, halt) and iti in [1,2]:
               extra_params = {'line': line, 'iti': iti, 'halt': halt}
-              xml_data = self.do_request('getwaitingtimes', extra_params)
-              return xml_data
+              response_data = self.do_request('getwaitingtimes', extra_params)
+              return response_data
 
       def get_itinerary(self, line=None, iti=1):
           '''Retrieve all stops for a line for a given direction (1 or 2)'''
           if line and iti in [1,2]:
               extra_params = {'line':line, 'iti':iti}
-              xml_data = self.do_request('getitinerary', extra_params)
+              response_data = self.do_request('getitinerary', extra_params)
               return xml_data
+
+      def get_json(self, json_data):
+          '''transform xml to dict to json'''
+          try:
+              json_data = xmltodict.parse(json_data)
+          except xmltodict.expat.ExpatError:
+              print("ERROR")
+              print(json_data)
+              return -1
+          json_data = json.dumps(json_data, ensure_ascii=False)
+          print(json_data)
+          return json_data
--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,11 @@
 from pystibmivb import Stibmivb
-import xmltodict
-import json
 
 stib = Stibmivb()
-
-
-gwt = stib.get_waiting_times(54,1,2955)
-gwt = xmltodict.parse(gwt)
+#stib.format = 'xml'
+print ("OK")
+g = stib.get_waiting_times(54,1,2955)
+'''gwt = xmltodict.parse(gwt)
 g = json.dumps(gwt, ensure_ascii=False)
+'''
 print(g)
+g = stib.get_waiting_times(54,2,2955)
